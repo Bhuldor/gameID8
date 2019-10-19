8ee236447df2208c12fbfdd2c488977c526680f7
Rodrigo Alvares <rsa.rodrigoalvares@gmail.com>
2019-10-19 18:52:48 -0300

    Floor spawner

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,25 +4,54 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour{
 
     /*##privateSettings##*/
+    //score
     public Text countText;
+
+    //floors
+    public GameObject floorsPrefab;
+    public float maxTime = 1f;
 
     /*##privateSettings##*/
     //score
     private float score = 0f;
     private float timer = 0f;
     private float nextActionTime = 0.0f;
-    public float period = 0.25f;
-
+    private float period = 0.25f;
+    //Spawn
+    private float x = -14f;
+    private float _timer = 0;
+    
+    private void Start(){
+        //Starts Floors
+        SpawnFloors(20);
+    }
 
     void Update () {
         if (Time.time > nextActionTime ) {
             nextActionTime += period;
             SetCountText ();
         }
+
+        if (_timer > maxTime){
+            SpawnFloors(1);
+            _timer = 0;
+        }
+        _timer += Time.deltaTime;
     }
 
     void SetCountText (){
         score++;
         countText.text = "Score: " + score.ToString ();
+    }
+
+    private void SpawnFloors(int count){
+        for (int c = 0; c < count; c++){
+            x += 2f;
+
+            var instance = Instantiate(floorsPrefab);
+            instance.transform.position = new Vector3(x, 0f, 0f);
+
+            Destroy(instance, 10f);
+        }
     }
 }
