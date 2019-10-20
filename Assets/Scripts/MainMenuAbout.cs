e4ecac5bf28f9f92955b2d011e19e5546e656faf
Rodrigo Santos Alvares <ralvares@EGSBR.local>
2019-10-20 16:22:51 -0300

    Tutorial and Stuffs

---
+++ Assets/Scripts/MainMenuAbout.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenuAbout : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            BackToMainMenu();
+        }
+    }
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
