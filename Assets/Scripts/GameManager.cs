eb0408d25ec13de35f9b0fecf1f7a29e9d2018d7
Rodrigo Santos Alvares <ralvares@EGSBR.local>
2019-10-20 04:17:36 -0300

    Vechicles prefabs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,6 +12,15 @@ public class GameManager : MonoBehaviour{
     public GameObject floorsFaixaPrefab;
     public float maxTime = 0.1f;
 
+    //Obstacles Prefabs
+    public GameObject bus;
+    public GameObject car;
+    public GameObject cavalete;
+    public GameObject poste;
+
+    //player
+    public GameObject player;
+
     //background
     public GameObject backgroundPrefab;
 
@@ -25,12 +34,18 @@ public class GameManager : MonoBehaviour{
     //score
     private float score = 0f;
     private float nextActionTimeBackground = 0.0f;
+
+    private float nextActionTimeObstacle = 0.0f;
     private float nextActionTime = 0.0f;
     private float period = 0.25f;
     private float periodBackground = 10f;
+    private float periodObstacle = 10f;
     //Spawn
     private float x = -22f;
     private float xBackground = 500f;
+    private float xObstacle = 0f;
+    private float yObstacle = 1.2f;
+    private float zObstacle = 0f;
     private int i;
     private int countWaitFaixa = 0;
     private bool faixaPut = false;
@@ -52,6 +67,11 @@ public class GameManager : MonoBehaviour{
             nextActionTimeBackground += periodBackground;
             SpawnBackground();
         }
+
+        if (Time.time > nextActionTimeObstacle ) {
+            nextActionTimeObstacle += periodObstacle;
+            SpawnRandomObstacle();
+        }
     }
 
     void SetCountText (){
@@ -64,6 +84,34 @@ public class GameManager : MonoBehaviour{
         instanceBackground.transform.position = new Vector3(xBackground, backgroundPrefab.transform.position.y, backgroundPrefab.transform.position.z);
 
         xBackground += 700f;
+    }
+
+    private void SpawnRandomObstacle(){
+        int spawnRandom = Random.Range(0,2);
+        Debug.Log("Random= " + spawnRandom);
+        GameObject instanceObstacle = bus;
+        xObstacle = player.transform.position.x + 30f;
+
+
+        switch (spawnRandom){
+            case 0:
+                instanceObstacle = bus;
+                yObstacle = -1.6f;
+                break;
+            case 1:
+                instanceObstacle = car;
+                yObstacle = 1.2f;
+                break;
+            /*case 2:
+                instanceObstacle = cavalete;
+                break;
+            case 3:
+                instanceObstacle = poste;
+                break;*/
+        }
+
+        var instance = Instantiate(instanceObstacle);
+        instance.transform.position = new Vector3(xObstacle, yObstacle, zObstacle);
     }
 
     public void SpawnFloors(int count){
